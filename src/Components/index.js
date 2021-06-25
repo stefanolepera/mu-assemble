d718b22059dd33c2c0c39eb961e3831593965211
stefanolepera <stefano.lepera@gamesys.co.uk>
2021-06-25 23:54:44 +0100

    build fixed

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,4 @@
 export { default as DropBox } from './DropBox/DropBox';
 export { default as Thumb } from './Thumb/Thumb';
 export { default as Image } from './Image/Image';
-export { default as Spinner } from './Spinner/Spinner';
 export { default as TeamComposition } from './TeamComposition/TeamComposition';
