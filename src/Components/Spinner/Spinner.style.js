494c9333c3ba952ee24529b5a3ddaa37a14f58b1
stefanolepera <stefano.lepera@gamesys.co.uk>
2021-06-29 16:26:02 +0100

    reverted Spinner color to maroon

--- src/Components/Spinner/Spinner.style.js
+++ src/Components/Spinner/Spinner.style.js
@@ -8,7 +8,7 @@ const spin = keyframes`
 export const StyledSpinner = styled.div`
     margin: ${props => props.marg} auto;
     border: 5px solid #ccc;
-    border-top: 5px solid #1251ff;
+    border-top: 5px solid maroon;
     border-radius: 50%;
     width: 50px;
     height: 50px;
